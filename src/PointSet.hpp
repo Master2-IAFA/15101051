1c305b827830392df629a6f2d71aea5902f1ebe0
Marina2468 <marina.ablaoui@gmail.com>
2022-10-05 15:24:39 +0200

    fix compilation error

--- src/PointSet.hpp
+++ src/PointSet.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <polyscope/polyscope.h>
 
 using std::vector;
 using std::string;
@@ -17,10 +18,10 @@ typedef struct  {
 class PointSet {
 public:
   PointSet (vector<point> _points) { m_points = _points; }
-  inline const *vector<point> getPoints { return &m_points; }
+  inline vector<point> getPoints () const& { return m_points; }
   void readPly (string filename);
   vector<point> getBoundingBox ();
 
 private:
-  std::vector<point> m_points;
+  vector<point> m_points;
 }
