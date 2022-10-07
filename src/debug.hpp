87d839a21b42cf44cb8a4090e34900414ab0e90e
Lou Denis <miss.lou.denis@gmail.com>
2022-10-07 13:50:06 +0200

    add drawCube

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,10 +1,14 @@
+#pragma once
 #include <iostream>
 #include <omp.h>
 #include <string>
 #include "polyscope/polyscope.h"
 #include "polyscope/point_cloud.h"
+#include "polyscope/curve_network.h"
 #include "PointSet.hpp"
 
 void pointSetToPolyscope(std::string name, PointSet *ps);
 
 std::vector<glm::vec3> build_cube_from_minmax(glm::vec3 min, glm::vec3 max);
+
+void drawCube(glm::vec3 min, glm::vec3 max);
