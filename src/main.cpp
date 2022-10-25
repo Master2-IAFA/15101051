105b44a0e5752b86f9ee4a7e762061fec6035a44
Antoine <antoine.lafon@univ-tlse3.fr>
2022-10-25 12:44:17 +0200

    better demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
 
 #include "polyscope/messages.h"
 #include "polyscope/point_cloud.h"
@@ -12,46 +14,55 @@
 
 #define MAX_DEPTH 7
 
+namespace fs = std::filesystem;
+
+std::string pathToDirectory{ "../assets/" };
+std::string path{"../assets/gaussian_spike_norm.ply"};
+std::vector<string> files;
+int current_item = 0;
 int depthToShow = 0;
+PointSet *ps;
+InputOctree *octree;
 std::array< polyscope::CurveNetwork*, MAX_DEPTH > octreeGraph;
 
 void showAtDepth( int depth );
+void loadPointCloud();
+void callback();
+
+int main(int argc, char **argv){
+
+    for (const auto & entry : fs::directory_iterator(pathToDirectory)){
+        std::string s = entry.path();
+        files.push_back( s );
+    }
+
+    polyscope::init();
+    ps = new PointSet();
+    ps->readOpenMesh( path );
+    loadPointCloud();
+    
+    polyscope::state::userCallback = callback;
+    polyscope::show();
+
+    delete ps;
+    delete octree;
+
+    return 0;
+}
+
+bool fileGetter(void *data, int index, const char** output)
+{
+    std::vector<string>* vec = (std::vector<string>*)data;
+    string &s = vec->at(index);
+    *output = s.c_str(); // not very safe
+    return true;
+}
 
 void callback(){
   ImGui::PushItemWidth( 100 );
   if(ImGui::SliderInt( "profondeur", &depthToShow, 0, MAX_DEPTH - 1 )) showAtDepth( depthToShow );
-
-}
-
-
-
-
-int main(int argc, char **argv){
-
-    polyscope::init();
-
-    PointSet *ps = new PointSet();
-    ps->readOpenMesh("../assets/bunny2.ply");
-
-    InputOctree *octree = generateInputOctree( MAX_DEPTH, ps );
-
-    for( int i = 0; i < MAX_DEPTH; i++ ){
-      auto o = octree->getAtDepth( i );
-      octreeGraph[i] = drawOctree( std::to_string(i), o );
-      octreeGraph[i]->setEnabled( false );
-    }
-
-    octreeGraph[0]->setEnabled( true );
-
-
-
-    
-
-    pointSetToPolyscope("gaussian", ps);
-    polyscope::state::userCallback = callback;
-    polyscope::show();
-
-    return 0;
+  if(ImGui::ListBox("files", &current_item, fileGetter, &files, files.size())){ path = files[current_item]; };
+  if(ImGui::Button("load file")) loadPointCloud();
 }
 
 void showAtDepth( int depth ){
@@ -59,4 +70,22 @@ void showAtDepth( int depth ){
     octreeGraph[i]->setEnabled( false );
   }
   octreeGraph[ depth ]->setEnabled( true );
+}
+
+void loadPointCloud(){
+    ps->readOpenMesh( std::string( path ) );
+    delete octree;
+    octree = generateInputOctree( MAX_DEPTH, ps );
+
+    for( int i = 0; i < MAX_DEPTH; i++ ){
+      auto o = octree->getAtDepth( i );
+      octreeGraph[i] = drawOctree( std::to_string(i), o );
+      octreeGraph[i]->setEnabled( false );
+    }
+
+    pointSetToPolyscope("pointCloud", ps);
+
+    octreeGraph[0]->setEnabled( true );
+
+    polyscope::view::resetCameraToHomeView();
 }
