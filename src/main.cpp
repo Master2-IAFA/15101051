08a1309fe7dcb4959ef91cfb762afba9e73a26c1
Marina2468 <marina.ablaoui@gmail.com>
2022-11-22 11:16:24 +0100

    revert to pre-merge main

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,17 @@
 #include "utils.t.hpp"
 #include "debug.hpp"
 
-#define MAX_DEPTH 3
+#define MAX_DEPTH 7
 
 namespace fs = std::filesystem;
 
 std::string pathToDirectory{ "../assets/" };
-std::string path{"../assets/bunny2.ply"};
+std::string path{"../assets/bunny_norm.ply"};
 std::vector<string> files;
 int current_item = 0;
 int depthToShow = 0;
 PointSet<point3d> *ps;
-PointSet<point2d> ps2d;
 Octree<statistics3d, glm::vec3> *octree;
-Octree<statistics2d, glm::vec2> *quad;
 std::array< polyscope::CurveNetwork*, MAX_DEPTH > octreeGraph;
 
 void showAtDepth( int depth );
@@ -32,39 +30,23 @@ void loadPointCloud();
 void callback();
 
 int main () {
-//    for (const auto & entry : fs::directory_iterator(pathToDirectory)){
-//        std::string s = entry.path();
-//        files.push_back( s );
-//    }
+    for (const auto & entry : fs::directory_iterator(pathToDirectory)){
+        std::string s = entry.path();
+        files.push_back( s );
+    }
 
     polyscope::init();
-    ps2d = generate2dGaussian();
-    quad = generateInputOctree<statistics2d, point2d, glm::vec2>( MAX_DEPTH, &ps2d );
+    ps = new PointSet<point3d>();
 
-    pointSet2dToPolyscope("point cloud", &ps2d);
-    for( int i = 0; i < MAX_DEPTH; i++ ) {
-        std::cout << "1" << std::endl;
-        auto o = quad->getAtDepth( i );
-        std::cout << "2" << std::endl;
-        octreeGraph[i] = drawQuadtree( std::to_string(i), o );
-        std::cout << "3" << std::endl;
-        octreeGraph[i]->setEnabled( false );
-        std::cout << "4" << std::endl;
-    }
-    //drawSquare("charabia", glm::vec2(1, 0), glm::vec2(0, 1));
-
-    octreeGraph[0]->setEnabled( true );
-
-    //loadPointCloud();
+    loadPointCloud();
 
     polyscope::state::userCallback = callback;
-    std::cout << "4" << std::endl;
+    pointSetToPolyscope("point cloud", ps);
 
     polyscope::show();
 
     delete ps;
     delete octree;
-    delete quad;
 
     return 0;
 }
@@ -80,8 +62,8 @@ bool fileGetter(void *data, int index, const char** output)
 void callback(){
     ImGui::PushItemWidth( 100 );
     if(ImGui::SliderInt( "profondeur", &depthToShow, 0, MAX_DEPTH - 1 )) showAtDepth( depthToShow );
-   // if(ImGui::ListBox("files", &current_item, fileGetter, &files, files.size())){ path = files[current_item]; };
-   // if(ImGui::Button("load file")) loadPointCloud();
+    if(ImGui::ListBox("files", &current_item, fileGetter, &files, files.size())){ path = files[current_item]; };
+    if(ImGui::Button("load file")) loadPointCloud();
 }
 
 void showAtDepth( int depth ){
