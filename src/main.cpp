e69858d72533260095b6e629d58297b1878f5a81
Marina2468 <marina.ablaoui@gmail.com>
2022-11-24 09:31:59 +0100

    update gui (load point cloud from ply)

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 #include "utils.t.hpp"
 #include "debug.hpp"
 
-#define MAX_DEPTH 3
+#define MAX_DEPTH 10
 
 namespace fs = std::filesystem;
 
@@ -21,21 +21,19 @@ std::string path{"../assets/bunny2.ply"};
 std::vector<string> files;
 int current_item = 0;
 int depthToShow = 0;
-PointSet<point3d> *ps;
 PointSet<point2d> ps2d;
-Octree<statistics3d, glm::vec3> *octree;
 Octree<statistics2d, glm::vec2> *quad;
 std::array< polyscope::CurveNetwork*, MAX_DEPTH > octreeGraph;
 
 void showAtDepth( int depth );
-void loadPointCloud();
+void loadPointCloud2D();
 void callback();
 
 int main () {
-//    for (const auto & entry : fs::directory_iterator(pathToDirectory)){
-//        std::string s = entry.path();
-//        files.push_back( s );
-//    }
+    for (const auto & entry : fs::directory_iterator(pathToDirectory)){
+        std::string s = entry.path();
+        files.push_back( s );
+    }
 
     polyscope::init();
     ps2d = generate2dGaussian();
@@ -44,43 +42,32 @@ int main () {
     pointSet2dToPolyscope("point cloud", &ps2d);
     for( int i = 0; i < MAX_DEPTH; i++ ) {
         auto o = quad->getAtDepth( i );
-        std::cout << "2" << std::endl;
         octreeGraph[i] = drawQuadtree( std::to_string(i), o );
-        std::cout << "3" << std::endl;
         octreeGraph[i]->setEnabled( false );
-        std::cout << "4" << std::endl;
     }
-    //drawSquare("charabia", glm::vec2(1, 0), glm::vec2(0, 1));
 
     octreeGraph[0]->setEnabled( true );
 
-    //loadPointCloud();
-
     polyscope::state::userCallback = callback;
-    std::cout << "4" << std::endl;
-
     polyscope::show();
 
-    delete ps;
-    delete octree;
     delete quad;
 
     return 0;
 }
 
-bool fileGetter(void *data, int index, const char** output)
-{
+bool fileGetter (void *data, int index, const char** output) {
     std::vector<string>* vec = (std::vector<string>*)data;
     string &s = vec->at(index);
     *output = s.c_str(); // not very safe
     return true;
 }
 
-void callback(){
-    ImGui::PushItemWidth( 100 );
+void callback () {
+    ImGui::PushItemWidth( 200 );
     if(ImGui::SliderInt( "profondeur", &depthToShow, 0, MAX_DEPTH - 1 )) showAtDepth( depthToShow );
-   // if(ImGui::ListBox("files", &current_item, fileGetter, &files, files.size())){ path = files[current_item]; };
-   // if(ImGui::Button("load file")) loadPointCloud();
+    if(ImGui::ListBox("files", &current_item, fileGetter, &files, files.size())){ path = files[current_item]; };
+    if(ImGui::Button("load file")) loadPointCloud2D();
 }
 
 void showAtDepth( int depth ){
@@ -90,19 +77,19 @@ void showAtDepth( int depth ){
     octreeGraph[ depth ]->setEnabled( true );
 }
 
-void loadPointCloud(){
-    ps->readOpenMesh( std::string( path ) );
-    delete octree;
+void loadPointCloud2D(){
+    ps2d.readOpenMesh( std::string( path ) );
+    delete quad;
 
-    octree = generateInputOctree<statistics3d, point3d, glm::vec3>( MAX_DEPTH, ps );
+    quad = generateInputOctree<statistics2d, point2d, glm::vec2>( MAX_DEPTH, &ps2d );
 
     for( int i = 0; i < MAX_DEPTH; i++ ){
-        auto o = octree->getAtDepth( i );
-        octreeGraph[i] = drawOctree( std::to_string(i), o );
+        auto o = quad->getAtDepth( i );
+        octreeGraph[i] = drawQuadtree( std::to_string(i), o );
         octreeGraph[i]->setEnabled( false );
     }
 
-    pointSetToPolyscope("point cloud", ps);
+    pointSet2dToPolyscope("point cloud", &ps2d);
 
     octreeGraph[0]->setEnabled( true );
 
