837e0d02d191efc0bb41d6e5644d9113d63fc7c6
Antoine_Lafon <antlaf46@gmail.com>
2023-01-26 13:45:01 +0100

    file selection gui

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 
 #include "Debug/ImguiInputOctreeDebug.t.hpp"
 #include "Debug/ImguiFittingDebug.t.hpp"
+#include "Debug/ImguiFileSelection.t.hpp"
 #include "ImguiDebug.t.hpp"
 
 #define MAX_DEPTH 7
@@ -29,9 +30,10 @@ namespace fs = std::filesystem;
 
 ImguiInputOctreeDebug *debug;
 ImguiFittingDebug *deebug;
+ImguiFileSelection<glm::vec3, statistics3d, point3d> *imguiFileSelection;
 
 std::string pathToDirectory{ "../assets/" };
-std::string path{ "../assets/bunny_30.ply" };
+std::string path{ "../assets/gaussian_spike_norm.ply" };
 std::vector<string> files;
 int current_item = 0;
 int depthToShow = 0;
@@ -51,17 +53,17 @@ void callback();
 
 int main () {
 
-    for (const auto & entry : fs::directory_iterator(pathToDirectory)){
-        std::string s = entry.path().string();
-        files.push_back( s );
-    }
+
 
     polyscope::init();
     ps = new PointSet<point3d>();
     loadPointCloud();
 
-    debug = new ImguiInputOctreeDebug( std::make_shared<InputOctree3D>( *octree ) );
-    deebug = new ImguiFittingDebug( std::make_shared<InputOctree3D>( *octree ) );
+    auto octreePtr = std::make_shared<InputOctree3D>( *octree );
+
+    imguiFileSelection = new ImguiFileSelection<glm::vec3, statistics3d, point3d>( ps, octreePtr, std::string("../assets/") );
+    debug = new ImguiInputOctreeDebug( octreePtr );
+    deebug = new ImguiFittingDebug( octreePtr );
 
     pc = pointSetToPolyscope("point cloud", ps);
     polyscope::state::userCallback = callback;
@@ -72,19 +74,15 @@ int main () {
     return 0;
 }
 
-bool fileGetter(void *data, int index, const char** output)
-{
-    std::vector<string>* vec = (std::vector<string>*)data;
-    string &s = vec->at(index);
-    *output = s.c_str(); // not very safe
-    return true;
-}
-
 void callback(){
     ImGui::PushItemWidth( 200 );
+
+    if( ImGui::CollapsingHeader("file selection") ) imguiFileSelection->draw();
+
     if( ImGui::CollapsingHeader("Octree parameters") ) debug->draw();
 
     if( ImGui::CollapsingHeader("Fitting") ) deebug->draw();
+
     //if(ImGui::SliderInt( "profondeur", &depthToShow, 0, MAX_DEPTH - 1 )) showAtDepth( depthToShow );
     // if(ImGui::ListBox("files", &current_item, fileGetter, &files, files.size())){ path = files[current_item]; };
     // if(ImGui::Button("load file")) loadPointCloud();
